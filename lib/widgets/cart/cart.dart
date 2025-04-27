467004f487e19099c67ede21acd4b53a5b73b322
tarikulabir <tarikulabir931@gmail.com>
2025-04-28 00:22:19 +0600

    Update design pattern

---
+++ lib/widgets/cart/cart.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class cart extends StatelessWidget {
+  const cart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Cart"),
+      ),
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
